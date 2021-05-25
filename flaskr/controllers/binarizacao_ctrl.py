a829f276821a9b77e7df71552fa05f97d56b721c
renantescaro <renantescaro@yahoo.com.br>
2021-05-24 23:03:18 -0300

    criando service de binarização

--- flaskr/controllers/binarizacao_ctrl.py
+++ flaskr/controllers/binarizacao_ctrl.py
@@ -1,4 +1,5 @@
 from flask import render_template, jsonify, Blueprint
+from PIL import ImageTk, Image
 from datetime import date, timedelta
 from flaskr.services.binarizacao_service import BinarizacaoSv
 
@@ -10,5 +11,16 @@ bp = Blueprint(
 class BinarizacaoCtrl:
     @bp.route('/binarizar', methods=['GET', 'POST'])
     def binarizar():
-        # BinarizacaoSv()
+        pillow_obj_inicial = Image.new("RGB", (100, 100))
+        imagem_inicial     = pillow_obj_inicial.load()
+
+        pillow_obj_final = Image.new("RGB", (100, 100))
+        imagem_final     = pillow_obj_final.load()
+
+        binarizacao = BinarizacaoSv(
+            linha=100,
+            coluna=100,
+            imagem_original=imagem_inicial,
+            imagem_final=imagem_final,
+            limiar=80 )
         return jsonify({'status':'ok'})
--- flaskr/services/binarizacao_service.py
+++ flaskr/services/binarizacao_service.py
@@ -1,4 +1,25 @@
 
 class BinarizacaoSv:
-    def __init__(self):
-        pass
+    def __init__(self, linha, coluna, imagem_original, imagem_final, limiar):
+        self._linha  = linha
+        self._coluna = coluna
+        self._imagem = imagem_original
+        self._limiar = limiar
+        self.imagem_final = imagem_final
+        self.processar()
+
+
+    def processar(self):
+        # linha
+        for i in range(self._linha):
+            # coluna
+            for j in range(self._coluna):
+                rgb_imagem = self._imagem.convert('RGB')
+                r, _, _ = rgb_imagem.getpixel((j, i))
+
+                cor = 0
+                if int(r) > int(self._limiar):
+                    cor = 255
+
+                self.imagem_final[j, i] = (cor, cor, cor)
+        return self.imagem_final
