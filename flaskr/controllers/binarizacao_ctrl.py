cddd0f593fea9a84006a26301135c504dabc7d6a
renantescaro <renantescaro@yahoo.com.br>
2021-05-25 13:24:36 -0300

    salvando imagem recebida pelo upload e ajustes parametros binarização

--- flaskr/controllers/binarizacao_ctrl.py
+++ flaskr/controllers/binarizacao_ctrl.py
@@ -1,4 +1,5 @@
-from flask import render_template, jsonify, Blueprint
+from flaskr.utils.debug import Debug
+from flask import request, jsonify, Blueprint
 from PIL import ImageTk, Image
 from datetime import date, timedelta
 from flaskr.services.binarizacao_service import BinarizacaoSv
@@ -9,18 +10,20 @@ bp = Blueprint(
     template_folder='templates' )
 
 class BinarizacaoCtrl:
-    @bp.route('/binarizar', methods=['GET', 'POST'])
+    @bp.route('/binarizar', methods=['POST'])
     def binarizar():
-        pillow_obj_inicial = Image.new("RGB", (100, 100))
-        imagem_inicial     = pillow_obj_inicial.load()
+        imagem = request.files['file']
+        if imagem.filename != '':
+            imagem.save('static/uploads/'+imagem.filename)
 
-        pillow_obj_final = Image.new("RGB", (100, 100))
-        imagem_final     = pillow_obj_final.load()
+        # abre imagem salva
+        imagem_original = Image.open('static/uploads/'+imagem.filename)
+        largura, altura = imagem_original.size
 
         binarizacao = BinarizacaoSv(
-            linha=100,
-            coluna=100,
-            imagem_original=imagem_inicial,
-            imagem_final=imagem_final,
-            limiar=80 )
+            linha           = altura,
+            coluna          = largura,
+            imagem_original = imagem_original,
+            limiar          = 80 )
+        binarizacao.processar()
         return jsonify({'status':'ok'})
